ea9ddf393855ed3e3a36c46868e4e3a8af59e237
Divyanshu charak <divyanshucharak14@gmail.com>
2024-03-08 11:36:19 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-//mongodb+srv://divyanshu:divyanshu@cluster0.lewitf4.mongodb.net/test
+
 
 mongoose.connect("mongodb+srv://divyanshu:divyanshu@cluster0.lewitf4.mongodb.net/todo")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
-    completed: Bolean
+    completed: Boolean
 })
 
 const todo = mongoose.model('todo', todoSchema);
