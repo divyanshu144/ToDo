0e63f5cafd860747fbd0ad9dd74c8655b38d1999
divyanshu144 <divyanshucharak14@gmail.com>
2024-03-08 11:29:24 +0530

    updated-backend-index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./typezod');
 const app = express()
 
 
@@ -7,12 +8,33 @@ app.use(express.json()) // express.json() -> middleware
 
 app.post("/todo", (req,res) => {
 
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    //put it in mongodb
 })
 
 app.get("/todos", (req,res) => {
+
+    
     
 })
 
 app.put("/completed", (req,res) => {
+
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "you sent the wrong inputs"
+        })
+        return
+    }
+    //mongodb
     
 })
